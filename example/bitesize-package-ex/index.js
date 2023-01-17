d5008f364d970480e6878a0b6adc7b9fbc3b6bac
isdiscodead <isdiscodead@korea.ac.kr>
2023-01-17 11:45:22 +0000

    node package

---
+++ example/bitesize-package-ex/index.js
@@ -0,0 +1,9 @@
+console.log("hello node.js package");
+
+const randomColor = require("randomcolor");
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+console.log(color1, color2, color3);
