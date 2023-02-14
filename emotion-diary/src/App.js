d3cf159f6daa553cf9b15d333ec5163fbb3127e9
isdiscodead <isdiscodead@gmail.com>
2023-02-14 17:26:16 +0000

    clean

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -5,61 +5,6 @@ import React from 'react';
 
 
 function App() {
-
-  // const [data, setData] = useState([]);
-	
-  const [data, dispatch] = useReducer(reducer, []);
-
-  const dataId = useRef(0);
-
-  const onCreate = useCallback((author, content, emotion) => {
-    
-	  dispatch({ type: 'CREATE', data: {author, content, emotion, id: dataId.current} })
-	  
-	  	/*
-	  	const created_date = new Date().getTime();
-	  
-		const newItem = {
-		  author,
-		  content,
-		  emotion,
-		  created_date,
-		  id: dataId.current
-		};
-		*/
-	  
-    // 개수 증가
-    dataId.current += 1;
-
-	// 함수형 업데이트로 최신 데이터 사용 가능하게끔 !! 
-    // setData((data) => [newItem, ...data]);
-	  
-  }, []); // 의존성 배열이 빈 배열이므로 mount 시에만 새로 ... 
-  
-  
-  const onRemove = useCallback(( targetId ) => {
-	  // console.log(`${targetId}가 삭제되었습니다.`);
-	  // setData(data => data.filter((it) => it.id !== targetId));
-	  dispatch({type: "REMOVE", targetId});
-  }, []);
-	
-	
-  const onEdit = useCallback((targetId, newContent) => {
-	  dispatch({ type: "EDIT", targetId, newContent });
-	  /*
-	  setData((data) =>
-		data.map((it) => 
-			// id가 일치하는 데이터를 찾아서 내용물만 바꿔줌! 일치하지 않을 경우엔 그대로 냅둠 ... 
-			it.id === target.id ? {...it, content: newContent } : it
-		)
-	  );
-	  */
-  }, []);
-	
-	const memoizedDispatches = useMemo(() => {
-		return { onCreate, onRemove, onEdit }
-	}, []);
-	
 	
 	const getDiaryAnalysis = () => {
 		// console.log("일기 분석 시작");
