c6ec1c25b8e30d9209b423d33e3aac7379b2c01d
isdiscodead <isdiscodead@gmail.com>
2023-02-16 17:27:07 +0000

    app test

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -6,20 +6,7 @@ import React from 'react';
 
 function App() {
   return (
-	<DiaryStateContext.Provider value={ data }>
-		  <DiaryDispatchContext.Provider value={ memoizedDispatches }>
-			<div className="App">
-			  {/* <Lifecycle /> */}
-			  {/* <OptimizeTest /> */}
-			  <DiaryEditor onCreate={onCreate} />
-				  <div>전체 일기 : {data.length}</div>
-				  <div>기분이 좋았던 일기 : {goodCount}</div>
-				  <div>기분이 나빴던 일기 : {badCount}</div>
-				  <div>기분 좋은 일기 비율 : {goodRatio}%</div>
-			  <DiaryList onRemove={onRemove} onEdit={onEdit} />
-			</div>
-		  </DiaryDispatchContext.Provider>
-    </DiaryStateContext.Provider>
+	  <div>cleaned</div>
   );
 };
 
